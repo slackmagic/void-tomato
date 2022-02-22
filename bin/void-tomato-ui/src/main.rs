b8a395faff183da66957b3e0fd3d1325429ff661
SlackMagiC <laurent.pietrzyk@gmail.com>
2022-02-22 22:15:44 +0100

    ✨ Notify

--- bin/void-tomato-ui/src/main.rs
+++ bin/void-tomato-ui/src/main.rs
@@ -1,6 +1,16 @@
+use notify_rust::{Hint, Notification};
 use void_tomato_lib::timer;
 
 #[tokio::main]
 async fn main() {
     timer::start().await;
+    Notification::new()
+        .summary("🍅 Category:email")
+        .body(
+            "This has nothing to do with emails.\nIt should not go away until you acknowledge it.",
+        )
+        .icon("thunderbird")
+        .appname("thunderbird")
+        .timeout(0) // this however is
+        .show();
 }
